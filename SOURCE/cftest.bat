b1966e7e215ddb52828dcfe61caf5bef8e9a22a3
kodakjerec0425 <kodakjerec@gmail.com>
2020-07-15 13:36:10 +0800

    ODBC 上線

---
+++ SOURCE/cftest.bat
@@ -0,0 +1,16 @@
+echo off
+NET TIME \\192.168.1.14 /SET /YES
+xcopy \\192.168.1.14\it\chanyun_system\cftest\*.pbd c:\cftest /Y/Q
+xcopy \\192.168.1.14\it\chanyun_system\cftest\*.exe c:\cftest /Y/Q
+xcopy \\192.168.1.14\it\chanyun_system\cftest\*.jpg c:\cftest /Y/Q
+
+del c:\cftest\*.txt /Q
+del c:\cftest\*.prn /Q
+del c:\cftest\*.dat /Q
+del c:\cftest\*.xls /Q
+
+ipconfig > c:\cftest\network.txt
+cd c:\cftest
+call cf.exe
+del c:\cftest\network.txt /Q
+echo on
